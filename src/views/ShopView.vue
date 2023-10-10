cc87f364486f58d8adf5777a51b44540c36cb386
GreggW <gwong@smartterm.io>
2023-10-10 00:24:35 -0500

    install bootstrap, initial changes

---
+++ src/views/ShopView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    Clicking this button will redirect you to the shop. You can return to the
+    homepage otherwise.
+    <a href="https://virginsintltrading.fygaroshops.com/products/">
+      Take me to the shop
+    </a>
+    <RouterLink :to="{ name: 'home' }">Return to home</RouterLink>
+  </div>
+</template>
+
+<script setup>
+import { onMounted } from "vue";
+
+// onMounted(() => {
+//   window.location.href = "https://virginsintltrading.fygaroshops.com/products/";
+//   //   alert("Works");
+// });
+</script>
+
+<style lang="scss" scoped></style>

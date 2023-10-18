5a4cf70594d2e004c55e3fae8927b6688fed0122
GreggW <gwong@smartterm.io>
2023-10-17 23:23:50 -0500

    Remove boilerplate links

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -16,43 +16,26 @@
 
         <nav id="navbar" class="navbar">
           <ul>
-            <li><a href="#hero" class="active">Home</a></li>
-            <li><a href="#about" class="">About</a></li>
-            <li><a href="#menu" class="">Menu</a></li>
-            <li><a href="#events">Events</a></li>
-            <li><a href="#chefs">Chefs</a></li>
-            <li><a href="#gallery">Gallery</a></li>
-            <li class="dropdown">
-              <a href="#"
-                ><span>Drop Down</span>
-                <i class="bi bi-chevron-down dropdown-indicator"></i
-              ></a>
-              <ul>
-                <li><a href="#">Drop Down 1</a></li>
-                <li class="dropdown">
-                  <a href="#"
-                    ><span>Deep Drop Down</span>
-                    <i class="bi bi-chevron-down dropdown-indicator"></i
-                  ></a>
-                  <ul>
-                    <li><a href="#">Deep Drop Down 1</a></li>
-                    <li><a href="#">Deep Drop Down 2</a></li>
-                    <li><a href="#">Deep Drop Down 3</a></li>
-                    <li><a href="#">Deep Drop Down 4</a></li>
-                    <li><a href="#">Deep Drop Down 5</a></li>
-                  </ul>
-                </li>
-                <li><a href="#">Drop Down 2</a></li>
-                <li><a href="#">Drop Down 3</a></li>
-                <li><a href="#">Drop Down 4</a></li>
-              </ul>
+            <li>
+              <RouterLink :to="{ name: 'home' }" class="active"
+                >Home</RouterLink
+              >
             </li>
-            <li><a href="#contact">Contact</a></li>
+            <li>
+              <RouterLink :to="{ name: 'about' }">About</RouterLink>
+            </li>
+            <li>
+              <RouterLink :to="{ name: 'contact' }">Contact Us</RouterLink>
+            </li>
+            <li>
+              <RouterLink :to="{ name: 'shop' }">Our Shop</RouterLink>
+            </li>
           </ul>
         </nav>
         <!-- .navbar -->
-
-        <a class="btn-book-a-table" href="#book-a-table">Book a Table</a>
+        <RouterLink :to="{ name: 'shop' }" class="btn-book-a-table"
+          >Our Shop</RouterLink
+        >
         <i class="mobile-nav-toggle mobile-nav-show bi bi-list"></i>
         <i class="mobile-nav-toggle mobile-nav-hide d-none bi bi-x"></i>
       </div>

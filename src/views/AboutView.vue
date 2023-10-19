44600acb21dbab85c1fe6dfc832d7b50f64ed085
Gregg Wong <gwong@smartterm.io>
2023-10-19 17:18:51 -0500

    Add content

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,14 +1,34 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about mx-5">
+   <section class="row">
+    <section class="col">
+      <h1>We're tightly connected to our communities</h1>
+    </section>
+    <section class="col">
+      <p>
+      Virgin’s international trading one of the leading distributors of aluminum products is located at 18 Chisholm Ave, Kingston 13 and was established out of a need to provide aluminum fabricators with an affordable alternative.
+We currently distribute aluminum extrusions in various colours for windows and doors, storefronts, signs, showers and showcases. We also have in stock a wide range of exotic tempered shower enclosures as well as products for Louvre windows, awnings, decorative aluminum fencing, fixed mesh frames, our exclusive retractable mesh frames and all related hardware.
+Virgin’s International Trading Limited was founded in 2005 by Garfield Virgin, who had a vision of providing high quality aluminum extrusion and hardware supplies at an affordable cost.
+Over the years Virgin’s International has consistently delivered on this promise and can now be considered as one of the leading aluminum extrusion company in Jamaica.
+We pride our self on excellent customer service by building strong relationships with our clients. The company has always emphasized customer satisfaction and systematically built a reputation for consistency by sourcing the highest quality products and reliability particularly in the area of island wide delivery.
+The job of guaranteeing a great customer experience comes through the combined efforts of our dedicated employees who work closely together to ensure that only the very best products and services reach our customers.
+
+    </p>
+    </section>
+   </section>
+   <section class="row">
+      <h1>
+        We're Making a Positive Impact Across Jamaica.
+      </h1>
+   </section>
   </div>
 </template>
 
 <style>
 @media (min-width: 1024px) {
   .about {
-    min-height: 60vh;
-    display: flex;
+    min-height: 100vh;
+
     align-items: center;
   }
 }

a5ab4c56ed46090860236b8f7f2c1a75f26f7292
Gregg Wong <gwong@smartterm.io>
2023-10-19 17:19:09 -0500

    Add Footer

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,14 @@
 import { RouterLink, RouterView } from "vue-router";
 import HelloWorld from "./components/HelloWorld.vue";
 import Navbar from "./components/Navbar.vue";
+import Footer from "./components/Footer.vue";
 </script>
 
 <template>
   <Navbar />
 
   <RouterView />
+  <Footer />
 </template>
 
 <style scoped>

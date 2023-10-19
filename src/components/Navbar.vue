93aab80a21d510dc6aff19b9f6f65c7a73d26520
Gregg Wong <gwong@smartterm.io>
2023-10-19 17:19:01 -0500

    Edit logo

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -10,8 +10,8 @@
           class="logo d-flex align-items-center me-auto me-lg-0"
         >
           <!-- Uncomment the line below if you also wish to use an image logo -->
-          <!-- <img src="assets/img/logo.png" alt=""> -->
-          <h1>Yummy<span>.</span></h1>
+          <img src="assets/img/logo.png" alt="">
+        
         </a>
 
         <nav id="navbar" class="navbar">

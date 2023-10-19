fd2c628bd41c4e8362fe6bfa12269649cfe6c7d1
Gregg Wong <gwong@smartterm.io>
2023-10-19 17:47:17 -0500

    Update map embed

--- src/views/ContactUsView.vue
+++ src/views/ContactUsView.vue
@@ -7,13 +7,11 @@
       </div>
 
       <div class="mb-3">
-        <iframe
-          style="border: 0; width: 100%; height: 350px"
-          src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621"
-          frameborder="0"
-          allowfullscreen=""
-        ></iframe>
+        <iframe style="border: 0; width: 100%; height: 350px"
+        src="https://maps.google.com/maps?q=18%20chisolm%20avenue&t=&z=13&ie=UTF8&iwloc=&output=embed"
+          frameborder="0" allowfullscreen=""></iframe>
       </div>
+    
       <!-- End Google Maps -->
 
       <div class="row gy-4">
@@ -22,7 +20,7 @@
             <i class="icon bi bi-map flex-shrink-0"></i>
             <div>
               <h3>Our Address</h3>
-              <p>A108 Adam Street, New York, NY 535022</p>
+              <p>18 Chisholm Avenue, Kingston 13</p>
             </div>
           </div>
         </div>
@@ -33,7 +31,7 @@
             <i class="icon bi bi-envelope flex-shrink-0"></i>
             <div>
               <h3>Email Us</h3>
-              <p>contact@example.com</p>
+              <p>sales@virginsintltrading.com</p>
             </div>
           </div>
         </div>
@@ -44,7 +42,7 @@
             <i class="icon bi bi-telephone flex-shrink-0"></i>
             <div>
               <h3>Call Us</h3>
-              <p>+1 5589 55488 55</p>
+              <p>(876) 754-1318 / 906-0736</p>
             </div>
           </div>
         </div>
@@ -65,52 +63,20 @@
         <!-- End Info Item -->
       </div>
 
-      <form
-        action="forms/contact.php"
-        method="post"
-        role="form"
-        class="php-email-form p-3 p-md-4"
-      >
+      <form action="forms/contact.php" method="post" role="form" class="php-email-form p-3 p-md-4">
         <div class="row">
           <div class="col-xl-6 form-group">
-            <input
-              type="text"
-              name="name"
-              class="form-control"
-              id="name"
-              placeholder="Your Name"
-              required=""
-            />
+            <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" required="" />
           </div>
           <div class="col-xl-6 form-group">
-            <input
-              type="email"
-              class="form-control"
-              name="email"
-              id="email"
-              placeholder="Your Email"
-              required=""
-            />
+            <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" required="" />
           </div>
         </div>
         <div class="form-group">
-          <input
-            type="text"
-            class="form-control"
-            name="subject"
-            id="subject"
-            placeholder="Subject"
-            required=""
-          />
+          <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" required="" />
         </div>
         <div class="form-group">
-          <textarea
-            class="form-control"
-            name="message"
-            rows="5"
-            placeholder="Message"
-            required=""
-          ></textarea>
+          <textarea class="form-control" name="message" rows="5" placeholder="Message" required=""></textarea>
         </div>
         <div class="my-3">
           <div class="loading">Loading</div>

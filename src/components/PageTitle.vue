ba84c5a1c188c561a9d982353e3ce503b8ca6840
GreggW <gwong@smartterm.io>
2023-10-19 20:00:12 -0500

    Add PageTitle component

---
+++ src/components/PageTitle.vue
@@ -0,0 +1,14 @@
+<template>
+  <section class="row">
+    <h1>{{ title }}</h1>
+  </section>
+</template>
+
+<script setup>
+const props = defineProps({
+  title: String,
+  required: false,
+});
+</script>
+
+<style lang="css" scoped></style>

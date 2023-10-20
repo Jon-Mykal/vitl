72a45e6ba131def0cd19d058201eae51b9bbd603
GreggW <gwong@smartterm.io>
2023-10-19 19:59:29 -0500

    Format content

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,37 +1,56 @@
+<script setup>
+import PageTitle from "../components/PageTitle.vue";
+</script>
+
 <template>
   <div class="about mx-5">
-   <section class="row">
-    <section class="col">
-      <h1>We're tightly connected to our communities</h1>
+    <PageTitle title="About" />
+    <section class="row">
+      <section class="col">
+        <h1>We're tightly connected to our communities</h1>
+      </section>
+      <section class="col">
+        <p>
+          Virgin’s international trading one of the leading distributors of
+          aluminum products is located at 18 Chisholm Ave, Kingston 13 and was
+          established out of a need to provide aluminum fabricators with an
+          affordable alternative. We currently distribute aluminum extrusions in
+          various colours for windows and doors, storefronts, signs, showers and
+          showcases. We also have in stock a wide range of exotic tempered
+          shower enclosures as well as products for Louvre windows, awnings,
+          decorative aluminum fencing, fixed mesh frames, our exclusive
+          retractable mesh frames and all related hardware. Virgin’s
+          International Trading Limited was founded in 2005 by Garfield Virgin,
+          who had a vision of providing high quality aluminum extrusion and
+          hardware supplies at an affordable cost. Over the years Virgin’s
+          International has consistently delivered on this promise and can now
+          be considered as one of the leading aluminum extrusion company in
+          Jamaica. We pride our self on excellent customer service by building
+          strong relationships with our clients. The company has always
+          emphasized customer satisfaction and systematically built a reputation
+          for consistency by sourcing the highest quality products and
+          reliability particularly in the area of island wide delivery. The job
+          of guaranteeing a great customer experience comes through the combined
+          efforts of our dedicated employees who work closely together to ensure
+          that only the very best products and services reach our customers.
+        </p>
+      </section>
     </section>
-    <section class="col">
-      <p>
-      Virgin’s international trading one of the leading distributors of aluminum products is located at 18 Chisholm Ave, Kingston 13 and was established out of a need to provide aluminum fabricators with an affordable alternative.
-We currently distribute aluminum extrusions in various colours for windows and doors, storefronts, signs, showers and showcases. We also have in stock a wide range of exotic tempered shower enclosures as well as products for Louvre windows, awnings, decorative aluminum fencing, fixed mesh frames, our exclusive retractable mesh frames and all related hardware.
-Virgin’s International Trading Limited was founded in 2005 by Garfield Virgin, who had a vision of providing high quality aluminum extrusion and hardware supplies at an affordable cost.
-Over the years Virgin’s International has consistently delivered on this promise and can now be considered as one of the leading aluminum extrusion company in Jamaica.
-We pride our self on excellent customer service by building strong relationships with our clients. The company has always emphasized customer satisfaction and systematically built a reputation for consistency by sourcing the highest quality products and reliability particularly in the area of island wide delivery.
-The job of guaranteeing a great customer experience comes through the combined efforts of our dedicated employees who work closely together to ensure that only the very best products and services reach our customers.
-
-    </p>
+    <section class="row">
+      <section class="col">
+        <img src="../assets/img/Impact image.jpg" alt="" />
+      </section>
+      <section class="col">
+        <h1>We're Making a Positive Impact Across Jamaica.</h1>
+        <p>
+          Virgin’s International Trading Limited is known for its community
+          support and civic duties. Our community work includes the adoption of
+          Top Hill Primary School in Manchester and donations to various police
+          community projects. We believe in serving our community for a better
+          tomorrow.
+        </p>
+      </section>
     </section>
-   </section>
-   <section class="row">
-
-    <section class="col">
-      <img src="../assets/img/Impact image.jpg" alt="">
-    </section>
-    <section class="col">
-      <h1>
-        We're Making a Positive Impact Across Jamaica.
-      </h1>
-      <p>
-        Virgin’s International Trading Limited is known for its community support and civic duties. 
-        Our community work includes the adoption of Top Hill Primary School in Manchester and donations to various police community projects. 
-        We believe in serving our community for a better tomorrow.
-      </p>
-    </section>
-   </section>
   </div>
 </template>
 

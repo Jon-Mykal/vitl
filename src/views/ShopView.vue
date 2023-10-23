5e3ed1142b7712f9c572b8ae469587ddd6a086ee
Gregg Wong <gwong@smartterm.io>
2023-10-23 13:10:27 -0500

    Add styling for shop page

--- src/views/ShopView.vue
+++ src/views/ShopView.vue
@@ -1,20 +1,26 @@
 <template>
-  <div class="shop">
-    Clicking this button will redirect you to the shop. You can return to the
-    homepage otherwise.
-    <a
-      class="btn-book-a-table"
-      href="https://virginsintltrading.fygaroshops.com/products/"
-    >
-      Take me to the shop
-    </a>
-    <RouterLink :to="{ name: 'home' }">Return to home</RouterLink>
-  </div>
+  <PageTitle title="Shop" />
+  <section>
+    <div class="shop d-flex justify-content-center">
+      <p class="lead">
+        Clicking the <strong>"Take me to the shop"</strong> button will redirect you to the shop. You can return to the
+        homepage otherwise.
+      </p>
+
+    </div>
+    <section class="d-flex justify-content-center pt-2">
+      <a class="btn-book-a-table fs-5" href="https://virginsintltrading.fygaroshops.com/products/">
+        Take me to the shop
+      </a>
+      <RouterLink class="return-home btn btn-link rounded text-decoration-none fs-5" :to="{ name: 'home' }">Return to home
+      </RouterLink>
+    </section>
+  </section>
 </template>
 
 <script setup>
 import { onMounted } from "vue";
-
+import PageTitle from "../components/PageTitle.vue";
 // onMounted(() => {
 //   window.location.href = "https://virginsintltrading.fygaroshops.com/products/";
 //   //   alert("Works");
@@ -22,11 +28,8 @@ import { onMounted } from "vue";
 </script>
 
 <style lang="css" scoped>
-@media (min-width: 1024px) {
-  .shop {
-    min-height: 60vh;
-    display: flex;
-    align-items: center;
-  }
+.return-home {
+  color: #ce1212;
+  ;
 }
 </style>

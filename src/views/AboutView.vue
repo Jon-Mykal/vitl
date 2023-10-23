87809ba5f1d9c45684dd69a6d024abb496ff6f6a
Gregg Wong <gwong@smartterm.io>
2023-10-23 17:37:26 -0500

    Add paragraphs

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -12,30 +12,42 @@ import image from  '../assets/img/Impact image.jpg'
         <h1 class="display-6 fw-bold primary">We're tightly connected to our communities</h1>
       </section>
       <section class="offset-sm-2 offset-md-0 col-sm-6 col-md-6 pt-1">
-        <p class="fs-5">
+
+         <p>
           Virgin’s international trading one of the leading distributors of
           aluminum products is located at 18 Chisholm Ave, Kingston 13 and was
           established out of a need to provide aluminum fabricators with an
-          affordable alternative. We currently distribute aluminum extrusions in
+          affordable alternative.
+         </p> <p>
+          We currently distribute aluminum extrusions in
           various colours for windows and doors, storefronts, signs, showers and
           showcases. We also have in stock a wide range of exotic tempered
           shower enclosures as well as products for Louvre windows, awnings,
           decorative aluminum fencing, fixed mesh frames, our exclusive
-          retractable mesh frames and all related hardware. Virgin’s
+          retractable mesh frames and all related hardware.
+         </p> <p>
+          Virgin’s
           International Trading Limited was founded in 2005 by Garfield Virgin,
           who had a vision of providing high quality aluminum extrusion and
-          hardware supplies at an affordable cost. Over the years Virgin’s
-          International has consistently delivered on this promise and can now
+          hardware supplies at an affordable cost.
+         </p>
+         <p>
+          Over the years Virgin’s International has consistently delivered on this promise and can now
           be considered as one of the leading aluminum extrusion company in
-          Jamaica. We pride our self on excellent customer service by building
+          Jamaica.
+         </p> <p>
+          We pride our self on excellent customer service by building
           strong relationships with our clients. The company has always
           emphasized customer satisfaction and systematically built a reputation
           for consistency by sourcing the highest quality products and
-          reliability particularly in the area of island wide delivery. The job
+          reliability particularly in the area of island wide delivery.
+         </p>  <p>
+          The job
           of guaranteeing a great customer experience comes through the combined
           efforts of our dedicated employees who work closely together to ensure
           that only the very best products and services reach our customers.
-        </p>
+         </p>
+        
       </section>
     </section>
     <section class="row pt-2 pt-sm-3">

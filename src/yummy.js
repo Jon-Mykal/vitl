16373ff045f9e75317439f27cd267c34c9346438
Gregg Wong <gwong@smartterm.io>
2023-10-23 15:50:49 -0500

    Add swiper styling

--- src/yummy.js
+++ src/yummy.js
@@ -9,6 +9,12 @@
 import PureCounter from "@srexi/purecounterjs";
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+// import Swiper and modules styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -116,6 +122,7 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
   /**
+   * 
    * Scroll top button
    */
   const scrollTop = document.querySelector('.scroll-top');
@@ -144,89 +151,89 @@ document.addEventListener('DOMContentLoaded', () => {
   /**
    * Init swiper slider with 1 slide at once in desktop view
    */
-  // new Swiper('.slides-1', {
-  //   speed: 600,
-  //   loop: true,
-  //   autoplay: {
-  //     delay: 5000,
-  //     disableOnInteraction: false
-  //   },
-  //   slidesPerView: 'auto',
-  //   pagination: {
-  //     el: '.swiper-pagination',
-  //     type: 'bullets',
-  //     clickable: true
-  //   },
-  //   navigation: {
-  //     nextEl: '.swiper-button-next',
-  //     prevEl: '.swiper-button-prev',
-  //   }
-  // });
+   new Swiper('.swiper', {
+    modules: [Navigation, Pagination],
+     speed: 600,
+     loop: true,
+    autoplay: {
+      delay: 2000
+    },
+     slidesPerView: 1,
+     pagination: {
+       el: '.swiper-pagination',
+       type: 'bullets',
+       clickable: true
+     },
+     navigation: {
+       nextEl: '.swiper-button-next',
+       prevEl: '.swiper-button-prev',
+     }
+   });
 
-  /**
-   * Init swiper slider with 3 slides at once in desktop view
-   */
-  // new Swiper('.slides-3', {
-  //   speed: 600,
-  //   loop: true,
-  //   autoplay: {
-  //     delay: 5000,
-  //     disableOnInteraction: false
-  //   },
-  //   slidesPerView: 'auto',
-  //   pagination: {
-  //     el: '.swiper-pagination',
-  //     type: 'bullets',
-  //     clickable: true
-  //   },
-  //   navigation: {
-  //     nextEl: '.swiper-button-next',
-  //     prevEl: '.swiper-button-prev',
-  //   },
-  //   breakpoints: {
-  //     320: {
-  //       slidesPerView: 1,
-  //       spaceBetween: 40
-  //     },
 
-  //     1200: {
-  //       slidesPerView: 3,
-  //     }
-  //   }
-  // });
+   // Init swiper slider with 3 slides at once in desktop view
+  
+   new Swiper('.slides-3', {
+     speed: 600,
+     loop: true,
+     autoplay: {
+       delay: 5000,
+       disableOnInteraction: false
+     },
+     slidesPerView: 'auto',
+     pagination: {
+       el: '.swiper-pagination',
+       type: 'bullets',
+       clickable: true
+     },
+     navigation: {
+       nextEl: '.swiper-button-next',
+       prevEl: '.swiper-button-prev',
+     },
+     breakpoints: {
+       320: {
+         slidesPerView: 1,
+         spaceBetween: 40
+       },
 
-  /**
-   * Gallery Slider
-   */
-  // new Swiper('.gallery-slider', {
-  //   speed: 400,
-  //   loop: true,
-  //   centeredSlides: true,
-  //   autoplay: {
-  //     delay: 5000,
-  //     disableOnInteraction: false
-  //   },
-  //   slidesPerView: 'auto',
-  //   pagination: {
-  //     el: '.swiper-pagination',
-  //     type: 'bullets',
-  //     clickable: true
-  //   },
-  //   breakpoints: {
-  //     320: {
-  //       slidesPerView: 1,
-  //       spaceBetween: 20
-  //     },
-  //     640: {
-  //       slidesPerView: 3,
-  //       spaceBetween: 20
-  //     },
-  //     992: {
-  //       slidesPerView: 5,
-  //       spaceBetween: 20
-  //     }
-  //   }
-  // });
+       1200: {
+         slidesPerView: 3,
+       }
+     }
+   });
+
+  
+  // Gallery Slider
+  
+   new Swiper('.gallery-slider', {
+     speed: 400,
+     loop: true,
+     centeredSlides: true,
+     autoplay: {
+       delay: 5000,
+       disableOnInteraction: false
+     },
+     slidesPerView: 'auto',
+     pagination: {
+       el: '.swiper-pagination',
+       type: 'bullets',
+       clickable: true
+     },
+     breakpoints: {
+       320: {
+         slidesPerView: 1,
+         spaceBetween: 20
+       },
+       640: {
+         slidesPerView: 3,
+         spaceBetween: 20
+       },
+       992: {
+         slidesPerView: 5,
+         spaceBetween: 20
+       }
+     }
+   });
 
   /**
    * Animation on scroll function and init

db073024a419ec611c3aefb1dd655a810f4009cc
GreggW <gwong@smartterm.io>
2023-10-25 00:14:19 -0500

    Update contact info, opening hours

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,75 +1,76 @@
 <template>
-   <footer id="footer" class="footer">
+  <footer id="footer" class="footer">
+    <div class="container">
+      <div class="row gy-3">
+        <div class="col-lg-3 col-md-6 d-flex">
+          <i class="bi bi-geo-alt icon"></i>
+          <div>
+            <h4>Address</h4>
+            <p>
+              18 Chisholm Avenue <br />
+              Kingston 13, Jamaica<br />
+            </p>
+          </div>
+        </div>
 
-<div class="container">
-  <div class="row gy-3">
-    <div class="col-lg-3 col-md-6 d-flex">
-      <i class="bi bi-geo-alt icon"></i>
-      <div>
-        <h4>Address</h4>
-        <p>
-            18 Chisholm Avenue <br>
-            Kingston 13, Jamaica<br>
-        </p>
+        <div class="col-lg-3 col-md-6 footer-links d-flex">
+          <i class="bi bi-telephone icon"></i>
+          <div>
+            <h4>Reservations</h4>
+            <p>
+              <strong>Phone:</strong> (876) 906-1934<br />
+              <strong>WhatsApp/Phone:</strong> (876) 335-9795<br />
+              <strong>Email:</strong> virgins.intltrading18@gmail.com<br />
+            </p>
+          </div>
+        </div>
+
+        <div class="col-lg-3 col-md-6 footer-links d-flex">
+          <i class="bi bi-clock icon"></i>
+          <div>
+            <h4>Opening Hours</h4>
+            <p>
+              <strong>Mon-Fri: 8AM</strong> - 5PM<br />
+              Saturday - Sunday: Closed
+            </p>
+          </div>
+        </div>
+
+        <div class="col-lg-3 col-md-6 footer-links">
+          <h4>Follow Us</h4>
+          <div class="social-links d-flex">
+            <!-- <a href="#" class="twitter"><i class="bi bi-twitter"></i></a> -->
+            <a
+              href="https://www.facebook.com/VirginsInternationalTradingLimited"
+              class="facebook"
+              ><i class="bi bi-facebook"></i
+            ></a>
+            <a
+              href="https://www.instagram.com/virginstrading/?hl=en"
+              class="instagram"
+              ><i class="bi bi-instagram"></i
+            ></a>
+            <!-- <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a> -->
+          </div>
+        </div>
       </div>
-
     </div>
 
-    <div class="col-lg-3 col-md-6 footer-links d-flex">
-      <i class="bi bi-telephone icon"></i>
-      <div>
-        <h4>Reservations</h4>
-        <p>
-          <strong>Phone:</strong> (876) 754-1318<br>
-          <strong>Email:</strong> sales@virginsintltrading.com<br>
-        </p>
+    <div class="container">
+      <div class="copyright">
+        © Copyright <strong><span>Virgin's International Trading</span></strong
+        >. All Rights Reserved
+      </div>
+      <div class="credits">
+        <!-- All the links in the footer should remain intact. -->
+        <!-- You can delete the links only if you purchased the pro version. -->
+        <!-- Licensing information: https://bootstrapmade.com/license/ -->
+        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/yummy-bootstrap-restaurant-website-template/ -->
       </div>
     </div>
-
-    <div class="col-lg-3 col-md-6 footer-links d-flex">
-      <i class="bi bi-clock icon"></i>
-      <div>
-        <h4>Opening Hours</h4>
-        <p>
-          <strong>Mon-Sat: 8AM</strong> - 5PM<br>
-          Sunday: Closed
-        </p>
-      </div>
-    </div>
-
-    <div class="col-lg-3 col-md-6 footer-links">
-      <h4>Follow Us</h4>
-      <div class="social-links d-flex">
-        <!-- <a href="#" class="twitter"><i class="bi bi-twitter"></i></a> -->
-        <a href="https://www.facebook.com/VirginsInternationalTradingLimited" class="facebook"><i class="bi bi-facebook"></i></a>
-        <a href="https://www.instagram.com/virginstrading/?hl=en" class="instagram"><i class="bi bi-instagram"></i></a>
-        <!-- <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a> -->
-      </div>
-    </div>
-
-  </div>
-</div>
-
-<div class="container">
-  <div class="copyright">
-    © Copyright <strong><span>Virgin's International Trading</span></strong>. All Rights Reserved
-  </div>
-  <div class="credits">
-    <!-- All the links in the footer should remain intact. -->
-    <!-- You can delete the links only if you purchased the pro version. -->
-    <!-- Licensing information: https://bootstrapmade.com/license/ -->
-    <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/yummy-bootstrap-restaurant-website-template/ -->
-
-  </div>
-</div>
-
-</footer>
+  </footer>
 </template>
 
-<script setup>
+<script setup></script>
 
-</script>
-
-<style lang="css" scoped>
-
-</style>
+<style lang="css" scoped></style>

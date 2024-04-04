3e0e506a0540a89abff4e05bc933f4816d7cb79c
Gregg Wong <gwong@smartterm.io>
2024-04-04 17:35:51 -0500

    Add product layout

--- src/components/ProductItem.vue
+++ src/components/ProductItem.vue
@@ -1,14 +1,59 @@
 <template>
-    <div>
-        <ul>
-            <li>
-        {{ product.name }}
-            </li>
-        </ul>
+ <div class="border border-neutral-200 rounded-md hover:shadow-lg max-w-[300px]">
+    <div class="relative">
+      <SfLink href="#" class="block">
+        <img
+          :src="product.imageSrc"
+          :alt="product.imageAlt"
+          class="block object-cover h-auto rounded-md aspect-square"
+          width="300"
+          height="300"
+        />
+      </SfLink>
+      <SfButton
+        variant="tertiary"
+        size="sm"
+        square
+        class="absolute bottom-0 right-0 mr-2 mb-2 bg-white ring-1 ring-inset ring-neutral-200 !rounded-full"
+        aria-label="Add to wishlist"
+      >
+        <SfIconFavorite size="sm" />
+      </SfButton>
     </div>
+    <div class="p-4 border-t border-neutral-200">
+      <SfLink href="#" variant="secondary" class="no-underline"> {{ product.name}} </SfLink>
+      <div class="flex items-center pt-1">
+        <SfRating size="xs" :value="5" :max="5" />
+
+        <SfLink href="#" variant="secondary" class="pl-1 no-underline">
+          <SfCounter size="xs">123</SfCounter>
+        </SfLink>
+      </div>
+      <p class="block py-2 font-normal leading-5 typography-text-sm text-neutral-700">
+        {{ product.description }}
+      </p>
+      <span class="block pb-2 font-bold typography-text-lg">${{ new Intl.NumberFormat('en-US').format(product.price)}}</span>
+      <SfButton size="sm" class="bg-black">
+        <template #prefix>
+          <SfIconShoppingCart size="sm" />
+        </template>
+        Add to cart
+      </SfButton>
+    </div>
+  </div>
 </template>
 
 <script setup>
+import {
+  SfAccordionItem,
+  SfCounter,
+  SfIconArrowBack,
+  SfIconCheck,
+  SfIconChevronLeft,
+  SfIconChevronRight, usePagination,
+  SfListItem,
+  SfRating, SfLink, SfButton, SfIconShoppingCart, SfIconFavorite
+} from '@storefront-ui/vue';
  const props = defineProps({
     product: Object
  })
